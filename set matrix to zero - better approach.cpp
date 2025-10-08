d9469e6f8c459be8567bf159cbb8d0b72ce0ba89
Aishix7 <hithaishi714@gmail.com>
2025-10-08 20:33:53 +0530

    time complexity is still O(n^2) but slightly better than brute force approach

--- set matrix to zero - better approach.cpp
+++ set matrix to zero - better approach.cpp
@@ -1,6 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+void check(int n,int m,vector<vector<int>> &matrix, vector<int> &Nrow, vector<int> &Ncol){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j] == 0){
+                Nrow[i] = 1;
+                Ncol[j%m] = 1;   
+            }
+        }
+    }
+}
+void change(int n,int m,vector<vector<int>> &matrix, vector<int> &Nrow, vector<int> &Ncol){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(Nrow[i] == 1){
+                matrix[i][j] = 0;
+            }
+            if(Ncol[i] == 1){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
 int main(){
     string line;
     vector<vector<int>>matrix;
@@ -23,9 +44,12 @@ int main(){
 
     int n = matrix.size();
     int m = matrix[0].size();
+    
+    vector<int>Nrow(n,0);
+    vector<int>Ncol(m,0);
 
-    check(n,m,matrix);
-    change(n,m,matrix);
+    check(n,m,matrix,Nrow,Ncol);
+    change(n,m,matrix,Nrow,Ncol);
 
     for(auto &row : matrix){
         for(auto val : row){
