e51c004bf9e5b07c26070d5dd96303dcfcaa079b
Aishix7 <hithaishi714@gmail.com>
2025-10-10 05:49:37 +0530

    time complexity of 2X O(n X m)

--- SetMatrixToZero-OptimalApproach.cpp
+++ SetMatrixToZero-OptimalApproach.cpp
@@ -1,3 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+void SetMatToZeros(int n,int m,vector<vector<int>> &matrix){
+    int col0 = 1;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j] == 0){
+
+                matrix[i][0] = 0;
+
+                if(j != 0) matrix[0][j] = 0;
+                else col0 = 0;
+                
+            }
+        }
+    }
+
+    for(int i=1;i<m;i++){
+        for(int j=1;j<m;j++){
+            if(matrix[i][j] != 0){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if(matrix[0][0] == 0){
+        for(int i=1;i<n;i++){
+            matrix[0][i] = 0;
+        }
+    }
+    if(col0 == 0){
+        for(int i=0;i<m;i++){
+            matrix[i][0] = 0;
+        }
+    }
+
+}
 int main(){
     string line;
     vector<vector<int>>matrix;
@@ -20,11 +58,7 @@ int main(){
     int n = matrix.size();
     int m = matrix[0].size();
     
-    vector<int>Nrow(n,0);
-    vector<int>Ncol(m,0);
-
-    check(n,m,matrix,Nrow,Ncol);
-    change(n,m,matrix,Nrow,Ncol);
+    SetMatToZeros(n,m,matrix)
 
     for(auto &row : matrix){
         for(auto val : row){
