a80b90b479247d36091d151a92b00fa66c246d4d
Aishix7 <hithaishi714@gmail.com>
2025-12-18 19:49:14 +0530

    time complexity is O(4n) and then space complexity is O(4n).

---
+++ SumMinimumInSubArray.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+int minimums(vector<int>& arr){
+    int n = arr.size();
+    stack<int>st;
+    
+    vector<int>NSR(n,n);
+    vector<int>NSL(n,-1);
+    
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && arr[st.top()] >= arr[i]){
+            st.pop();
+        }
+        if(!st.empty()){
+            NSR[i] = st.top();
+        }
+        st.push(i);
+    }
+    
+    while(!st.empty()){
+        st.pop();
+    }
+
+    for(int i=0;i<n;i++){
+        while(!st.empty() && arr[st.top()] > arr[i]){
+            st.pop();
+        }
+        if(!st.empty()){
+            NSL[i] = st.top();
+        }
+        st.push(i);
+    }
+    
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum += (arr[i]*((i-NSL[i])*(NSR[i] - i)));
+    }
+    return sum;
+    
+    
+}
+int main()
+{
+    vector<int> arr = {3,1,2,4};
+    int sum = minimums(arr);
+    cout << sum <<"\n";
+
+    return 0;
+}
